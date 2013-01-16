5f363638fd77d2c4c2c64f9535a7288317dbfe1b
philipho <pho147@gmail.com>
2013-01-16 20:51:17 +0000

    Added new file TweetDataFromTwitterStreamTest.java

---
+++ TwitterDataPublisher/src/test/mec/datareceiver/TweetDataFromTwitterStreamTest.java
@@ -0,0 +1,15 @@
+package test.mec.datareceiver;
+
+public class TweetDataFromTwitterStreamTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
